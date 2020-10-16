ad9182dddb79e60753e87133df812881cec5b9fc
Patrick Verbrugge <patrickv852@gmail.com>
2020-10-15 19:00:59 -0500

    Added seeds sent to seed box in loottracker

--- runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/loottracker/LootTrackerPlugin.java
@@ -153,6 +153,9 @@ public class LootTrackerPlugin extends Plugin
 	private static final String HERBIBOAR_EVENT = "Herbiboar";
 	private static final Pattern HERBIBOAR_HERB_SACK_PATTERN = Pattern.compile(".+(Grimy .+?) herb.+");
 
+	// Master Farmer loot handling
+	private static final Pattern MASTER_FARMER_SEED_BOX_PATTERN = Pattern.compile("The following stolen loot gets added to your seed box: (.+?) x (\\d+)\\.");
+
 	// Seed Pack loot handling
 	private static final String SEEDPACK_EVENT = "Seed pack";
 
@@ -1069,6 +1072,13 @@ public class LootTrackerPlugin extends Plugin
 			return;
 		}
 
+		Matcher seedBoxMatcher = MASTER_FARMER_SEED_BOX_PATTERN.matcher(message);
+		if (seedBoxMatcher.matches())
+		{
+			processMasterFarmerSeedBoxLoot(seedBoxMatcher);
+			return;
+		}
+
 		final Matcher pickpocketMatcher = PICKPOCKET_REGEX.matcher(message);
 		if (pickpocketMatcher.matches())
 		{
@@ -1516,6 +1526,16 @@ public class LootTrackerPlugin extends Plugin
 		return true;
 	}
 
+	private boolean processMasterFarmerSeedBoxLoot(Matcher matcher)
+	{
+		int quantityStolen = Integer.parseInt(matcher.group(2));
+		List<ItemStack> seeds = Collections.singletonList(new ItemStack(itemManager.search(matcher.group(1)).get(0).getId(), quantityStolen, client.getLocalPlayer().getLocalLocation()));
+
+		int thievingLevelLevel = client.getBoostedSkillLevel(Skill.THIEVING);
+		addLoot(lastPickpocketTarget, -1, LootRecordType.PICKPOCKET, thievingLevelLevel, seeds);
+		return true;
+	}
+
 	@VisibleForTesting
 	boolean inTobChestRegion()
 	{
@@ -1715,3 +1735,4 @@ public class LootTrackerPlugin extends Plugin
 		}
 	}
 }
+
